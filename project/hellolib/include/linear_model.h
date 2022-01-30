61fb4f5609ddd5e22c59cde52ec61109a3f144c3
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2022-01-30 11:08:55 +0100

    iter

---
+++ project/hellolib/include/linear_model.h
@@ -0,0 +1,12 @@
+#include "linear_model_export.h"
+
+namespace linear_model {
+class LINEAR_MODEL_EXPORT LinearRegression {
+ public:
+  bool fit_intercept;
+
+ public:
+  LinearRegression();
+};
+
+}  // namespace linear_model
---
+++ project/hellolib/src/linear_model.cpp
@@ -0,0 +1,3 @@
+#include "linear_model.h"
+
+linear_model::LinearRegression() { fit_intercept = true; }
