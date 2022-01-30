3fe3fc5700d7d73b23ced4564ae5d15cf3c5d561
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2022-01-30 19:17:12 +0100

    iter

--- project/mllib/test/linear_model.cpp
+++ project/mllib/test/linear_model.cpp
@@ -1,9 +1,32 @@
+#include "linear_model.h"
+
 #include <gtest/gtest.h>
 
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+#include <random>
+#include <tuple>
+
+using namespace mllib;
+
+std::tuple<MatrixData, VectorTarget> generate_random_data() {
+  const int n_samples = 100, n_features = 2;
+  const double mean = 0.0, std_dev = 1.0;
+
+  std::default_random_engine generator;
+  std::normal_distribution<double> distribution(mean, std_dev);
+
+  Eigen::Matrix<double, n_samples, n_features> X;
+  Eigen::Vector<double, n_samples> y;
+
+  for (auto sample_it : X.rowwise()) {
+    for (auto feature_it = sample_it.begin(); feature_it != sample_it.end();
+         ++feature_it) {
+      *feature_it = distribution(generator);
+    }
+  }
+  for (auto sample_it = y.begin(); sample_it != y.end(); ++sample_it) {
+    *sample_it = distribution(generator);
+  }
+  return {X, y};
 }
+
+TEST(LineaRegression, SmokeTest) { linear_model::LinearRegression lr(); }
